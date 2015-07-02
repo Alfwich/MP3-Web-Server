06c631d67f7b56436308a8b6c265fea196d6d412
Arthur Wuterich <arthur@vertigo.com>
2015-07-02 10:37:00 -0700

    Refactoring client

--- python/src/js/player.js
+++ python/src/js/player.js
@@ -1,91 +1,102 @@
-app.controller('main', function($scope) {
+(function(){
 
-  $scope.currentState = {};
-  $scope.requests = [];
-  $scope.requestUrl = "index.html";
+  var processTime = function(t) {
+      var result = null;
 
-  $scope.callbacks = {
-    "next_album" : function(data) {
-      $scope.syncPlayer();    
-    },
-    
-    "prev_album" : function(data) {
-      $scope.syncPlayer();    
-    },
+      if( t && typeof t === "string" ) {
+        var comps = t.split(":");
+        if( comps.length == 2 ) {
+          result = parseInt(comps[0])*60 + parseInt(comps[1]);
+        }
+      }
 
-    "refresh_data" : function() {
-      $scope.syncPlayer();
-    }
-
+      return result;
   }
-  
-  $scope.processTime = function(t) {
-    var result = null;
-    
-    if( t && typeof t === "string" ) {
-      var comps = t.split(":");
-      if( comps.length == 2 ) {
-        result = parseInt(comps[0])*60 + parseInt(comps[1]);
+
+  app.controller('main', function($scope) {
+
+    $scope.currentState = {};
+    $scope.requests = [];
+    $scope.requestUrl = "index.html";
+
+    // Get the saved requestUrl from localstorage if it exists
+    if( localStorage ) {
+      var item;
+      if( item = localStorage.getItem("MP3-OVERRIDE-URL")) {
+        $scope.requestUrl = item;
       }
     }
-  
-    return result;
-  }
-  
-  $scope.computeDuration = function() {
-    var currentTime = $scope.processTime( $scope.currentState.currenttime ),
-        totalTime = $scope.processTime( $scope.currentState.totaltime ),
-        result = 0;
-    
-    if( currentTime !== null && totalTime !== null ) {
-      result = currentTime / totalTime * 100;
-    }
-    
-    return result + "%";
-  }
-  
-  // Wrapper for button clicks to disable actions 
-  // until all of the requests are resolved
-  $scope.buttonPlayerAction = function( action, callback ) {
-    if( !$scope.requests.length ) {
-      $scope.playerAction( action, callback )
-    }
-  }
-   
-  // Perform a network action with the server
-  $scope.playerAction = function( action, callback) {
-    if( typeof callback === "undefined" ) {
-      callback = function(d){ console.log(d); };
-    }
-        
-    var request = $.ajax({
-      type: "POST",
-      url: $scope.requestUrl,
-      data: { action: action },
-      timeout: 5000
-    })
-    .done(callback)
-    .always(function(data){
-      // Provide a action callback if it is defined
-      if( typeof $scope.callbacks[action] === "function" ) {
-        $scope.callbacks[action]( data );
+
+    $scope.callbacks = {
+      "next_album" : function(data) {
+        $scope.syncPlayer();
+      },
+
+      "prev_album" : function(data) {
+        $scope.syncPlayer();
+      },
+
+      "refresh_data" : function() {
+        $scope.syncPlayer();
       }
-      $scope.requests.pop();
-      $scope.$apply();
-    });
-    
-    $scope.requests.push( request );
-  };
-  
-  $scope.syncPlayer = function(){
-    $scope.playerAction( "info", function(data){
-      if( data && data["output"] ) {
-        setTimeout( $scope.syncPlayer, 2000 );
-        $scope.currentState = data["output"];
+
+    }
+
+    $scope.computeDuration = function() {
+      var currentTime = processTime( $scope.currentState.currenttime ),
+          totalTime = processTime( $scope.currentState.totaltime ),
+          result = 0;
+
+      if( currentTime !== null && totalTime !== null ) {
+        result = currentTime / totalTime * 100;
+      }
+
+      return result + "%";
+    }
+
+    // Wrapper for button clicks to disable actions
+    // until all of the requests are resolved
+    $scope.buttonPlayerAction = function( action, callback ) {
+      if( !$scope.requests.length ) {
+        $scope.playerAction( action, callback )
+      }
+    }
+
+    // Perform a network action with the server
+    $scope.playerAction = function( action, callback) {
+      if( typeof callback === "undefined" ) {
+        callback = function(d){ console.log(d); };
+      }
+
+      var request = $.ajax({
+        type: "POST",
+        url: $scope.requestUrl,
+        data: { action: action },
+        timeout: 5000
+      })
+      .done(callback)
+      .always(function(data){
+        // Provide a action callback if it is defined
+        if( typeof $scope.callbacks[action] === "function" ) {
+          $scope.callbacks[action]( data );
+        }
+        $scope.requests.pop();
         $scope.$apply();
-      }
-    });   
-  };
-    
-  setTimeout( $scope.syncPlayer, 2000 );
-});
+      });
+
+      $scope.requests.push( request );
+    };
+
+    $scope.syncPlayer = function(){
+      $scope.playerAction( "info", function(data){
+        if( data && data["output"] ) {
+          setTimeout( $scope.syncPlayer, 2000 );
+          $scope.currentState = data["output"];
+          $scope.$apply();
+        }
+      });
+    };
+
+    setTimeout( $scope.syncPlayer, 2000 );
+  });
+})()
